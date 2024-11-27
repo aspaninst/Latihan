463378662e51b2528b1a94269113c8181aad735e
aspaninst <aspanasutionn@gmail.com>
2024-11-28 03:08:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -121,26 +121,24 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+// Fungsi untuk menampilkan atau menyembunyikan dropdown menu
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
+// Menutup dropdown jika pengguna mengklik di luar dropdown
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
-  }
 }
+
 
 // Fungsi untuk membuka popup form Join
 document.getElementById('joinBtn').addEventListener('click', function() {
